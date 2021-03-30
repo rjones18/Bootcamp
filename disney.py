bd9cc3b700a5e57c7de1b2cf86d1a55e3d044b68
Reggie Jones <64709385+rjones18@users.noreply.github.com>
2021-03-29 21:39:54 -0400

    Create disney.py

---
+++ disney.py
@@ -0,0 +1,11 @@
+disney_characters = ['simba', 'ariel', 'pumba', 'flounder', 'nala', 'ursula', 'scar', 'flotsam', 'timon']
+
+for disney in disney_characters:
+    if 'u' in disney:
+        print(f"{disney}, U are so Uniquely U!")
+    elif 'i' in disney:
+        print(f"{disney}, I bet you're Impressively Intelligent!")
+    elif 'o' in disney:
+        print(f"{disney}, O My! How Original!")
+    else:
+        print(f"{disney}, Ehh, a's and e's are so ordinary.")
